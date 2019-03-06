856b7921f75246984e608df7b6d27342cacf16b6
Carlos Nah <cnah27@gmail.com>
2019-03-06 16:37:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // add solution here
+
+function theBeaatlesPlay(musicians,instrunents) {
+  let new_array = []
+  
+  for(let i = 0; i < musicians.length; i++) {
+    new_array.push(musicians[i] +" plays " + instruments[i])
+  }
+  
+  return new_array
+}
