ffd12a5ca2208a4ba6b2ec0bf57b678d4de39c16
Carlos Nah <cnah27@gmail.com>
2019-03-06 16:38:56 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ function theBeatlesPlay(musicians,instrunents) {
   let new_array = []
   
   for(let i = 0; i < musicians.length; i++) {
-    new_array.push(musicians[i] +" plays " + instruments[i])
+    new_array.push(musicians[i] +" plays " + instruments[i]);
   }
   
   return new_array
